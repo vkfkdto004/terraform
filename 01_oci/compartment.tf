e0c481d4b726c3c203d47272911e2d3c34b6bfee
wooseop.kim <125433456+vkfkdto004@users.noreply.github.com>
2023-10-21 22:43:37 +0900

    Delete 01_oci directory

--- 01_oci/compartment.tf
+++
@@ -1,7 +0,0 @@
-# create compartment
-resource "oci_identity_compartment" "compartment" {
-    compartment_id = var.parent_compartment_ocid
-    description = var.compartment_description
-    name = var.compartment_name
-    enable_delete = var.compartment_enable_delete
-}
--- 01_oci/lb.tf
+++
@@ -1,8 +0,0 @@
-resource "oci_load_balancer" "lb" {
-  shape = "100Mbps"
-  compartment_id = var.parent_compartment_ocid
-
-  subnet_ids = [oci_core_subnet.id]
-
-  display_name = "lb"
-}
